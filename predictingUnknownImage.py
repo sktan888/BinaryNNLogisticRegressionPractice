c77f25bf4d909da2b11388ce9edfc21199e66555
sktan888 <402duchess@gmail.com>
2024-11-24 03:40:32 +0000

    cli pred

--- predictingUnknownImage.py
+++ predictingUnknownImage.py
@@ -1,41 +1,34 @@
-# pylint: disable=pointless-statement, unused-variable
-#!/usr/bin/env python3
-
-# python predictingUnknownImage.py predict-cmd Image where Image is the filename nine.jpg uploaded in assets/images folder
-
-import numpy as np
-from myLib.helper import predict
-from myLib.mylog import log
-import matplotlib.pyplot as plt
-from PIL import Image
-
 import click
+from myLib.mylog import log
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from myLib.helper import predict
 
 
-@click.group()
-def cli():
-    """run NN Prediction"""
-
-
-@cli.command()
-@click.argument("fileName")  # the name of the image file
-def predict_cmd(fileName):
-    """predict a given image"""
-
+@click.command()
+@click.argument("file_name")
+def predicting(file_name):
+    """Predict"""
     # injest test datasets from the NPY file
-    test_set_x = np.load("test_set_x.npy")
+    # test_set_x = np.load("test_set_x.npy")
     num_px = 28  # test_set_x.shape[1]
     log("num_px " + str(num_px))
 
     # injest image file my_image.jpg
     # Preprocess the image to fit  the NN algorithm.
-    fname = "assets/images/" + fileName
+    fname = "assets/images/" + file_name
     image = np.array(Image.open(fname).resize((num_px, num_px)))
+    image = image[:, :, 0]
+    # plt.imshow(image)
     log("image.shape " + str(image.shape))
+    click.echo(f"image.shape " + str(image.shape))
 
-    plt.imshow(image)
     image = image / 255.0
-    image = image.reshape((1, num_px * num_px * 3)).T
+    image = image.reshape((1, num_px * num_px)).T
+
+    log("image.shape " + str(image.shape))
+    click.echo(f"image.shape " + str(image.shape))
 
     # Load trained model from the NPY file
     w = np.load("model_weights.npy")
@@ -43,12 +36,12 @@ def predict_cmd(fileName):
         0
     ]  # convert a Python array with a single element to a scalar
 
-    a = "Actual = " + str(fileName)
+    a = "Actual = " + str(file_name)
     p = "Prediction = " + str(predict(w, b, image))
     click.echo(a)
     click.echo(p)
-    log("Example " + str(fileName) + " :: " + a + " : " + p)
+    log("Example " + str(file_name) + " :: " + a + " : " + p)
 
 
 if __name__ == "__main__":
-    cli()
+    predicting()
