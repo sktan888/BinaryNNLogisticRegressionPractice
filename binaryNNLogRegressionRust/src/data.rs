7816b7c3990f6acd637b02038ce429747b4227c6
sktan888 <402duchess@gmail.com>
2024-11-24 10:37:45 +0000

    rust

--- binaryNNLogRegressionRust/src/data.rs
+++ binaryNNLogRegressionRust/src/data.rs
@@ -1,11 +1,28 @@
-// A module named `my_mod`
+use mnist::{Mnist, MnistBuilder};
 
 fn private_fun() {
     println!("called `my_mod::private_function()`");
 }
 
-// Use the `pub` modifier to override default visibility.
-pub fn fun() {
-    println!("called `my_mod::function()`");
-    private_fun();
+// Loading data for handwriting
+pub fn injest(digit: i32) {
+
+    // private_fun();
+
+    // Load the MNIST dataset
+    let mnist = MnistBuilder::default()
+    .label_format(mnist::LabelFormat::OneHot)
+    .finalize()
+    .expect("Error initializing MNist");
+
+    // Get the training and test sets
+    let (x_train, y_train, x_test, y_test) = mnist.load_mnist(None).expect("Error loading MNIST");
+
+    // Print the shape of the training data
+    println!("Shape of x_train: {:?}", x_train.shape());
+    println!("Shape of y_train: {:?}", y_train.shape());
 }
+
+
+
+
--- binaryNNLogRegressionRust/src/main.rs
+++ binaryNNLogRegressionRust/src/main.rs
@@ -1,3 +1,9 @@
+
+
+mod data;
+use crate::data::injest;
+
 fn main() {
     println!("Hello, world!  bb");
+    injest(2);
 }
