35c64e39f8369018ae483eef7fbfe20045defc25
sktan888 <402duchess@gmail.com>
2024-11-26 23:35:04 +0000

    data.rs

--- binaryNNLogRegressionRust/src/data.rs
+++ binaryNNLogRegressionRust/src/data.rs
@@ -8,6 +8,10 @@ use ndarray::prelude::*;
 pub fn injest(_digit: i32) {
     // The default path to the MNIST data files is /data of top layer crate
 
+    let m_train = 60_000;
+    let m_test = 10_000;
+    let _num_px = 28;
+
     // Download and Load the MNIST dataset
     let Mnist {
         trn_img,
@@ -17,10 +21,10 @@ pub fn injest(_digit: i32) {
         ..
     } = MnistBuilder::new()
         .label_format_digit()
-        .training_set_length(60_000)
-        .test_set_length(10_000)
+        .training_set_length(m_train)
+        .test_set_length(m_test)
         .finalize();
-    //.validation_set_length(10_000)
+    //.validation_set_length(m_test)
     let _image_num = 0;
     // Can use an Array2 or Array3 here (Array3 for visualization)
     let train_data = Array3::from_shape_vec((60_000, 28, 28), trn_img)
@@ -49,7 +53,32 @@ pub fn injest(_digit: i32) {
 
     // Print the shape of the training data
     println!("Shape of train_data: {:?}", train_data.shape());
-    println!("Shape of train_labels: {:?}", train_labels.shape());
     println!("Shape of test_data: {:?}", test_data.shape());
+
+
+
+    // # train set y and test_set_y are originally row vector (m, 1) array. Reshape to column vector (1,m) array
+    println!("Shape of train_labels: {:?}", train_labels.shape());
     println!("Shape of test_labels: {:?}", test_labels.shape());
+    println!("Element of train_labels: {:?}", train_labels[(0,0)]);
+
+    // Reshape the ArrayView1 into a 2D array
+    let _shape = (1, m_train); // Reshape into a (1,m) matrix
+    let train_labels_colvector = train_labels.into_shape_with_order((1,60_000)).unwrap();
+    println!("Shape of train_labels_colvector: {:?}", train_labels_colvector.shape());
+
+    let test_labels_colvector = test_labels.into_shape_with_order((1,10_000)).unwrap();
+    println!("Shape of test_labels_colvector: {:?}", test_labels_colvector.shape());
+    
+    // Flatten array(60_000, 28, 28) into (:,60000)
+    let nested_vec = vec![vec![1, 2, 3], vec[4, 5, 6]];
+    let flattened_vec: Vec<i32> = nested_vec.into_iter().flatten().collect();
+    println!("Shape of nested_vec: {:?}", nested_vec.shape());
+    println!("Shape of flattened_vec: {:?}", flattened_vec.shape());
+
+    // Access the dimension information
+    /*
+    let dim = arr.dim(); // This is of type `ndarray::Dim<[usize; 3]>`
+    let (x, y, z) = dim.into_tuple(); // Extract dimensions as tuple
+    */
 }
